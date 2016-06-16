0d7c7968c6e6a7154e3febb31859e89a7301e359
sunny-hong <sunny@gatech.edu>
2016-06-16 11:26:47 -0400

    Initial Commit

---
+++ lecture4Stanford/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lecture4Stanford
+//
+//  Created by Hye Sun Hong on 6/16/16.
+//  Copyright © 2016 Vaporware. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ lecture4StanfordTests/lecture4StanfordTests.swift
@@ -0,0 +1,36 @@
+//
+//  lecture4StanfordTests.swift
+//  lecture4StanfordTests
+//
+//  Created by Hye Sun Hong on 6/16/16.
+//  Copyright © 2016 Vaporware. All rights reserved.
+//
+
+import XCTest
+@testable import lecture4Stanford
+
+class lecture4StanfordTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
